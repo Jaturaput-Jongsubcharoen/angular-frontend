5eb9ff94b7c1428e4b12820417fae4646b4c0c6b
Jaturaput-Jongsubcharoen <macker.jong@gmail.com>
2026-01-03 22:28:33 -0500

    decoration

--- src/app/components/fruits/fruits.ts
+++ src/app/components/fruits/fruits.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { FruitsService } from '../../services/fruits';
+import { HighlightDirective } from '../../directives/highlight';
 
 @Component({
   selector: 'app-fruits',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, HighlightDirective],
   templateUrl: './fruits.html',
   styleUrl: './fruits.css'
 })
